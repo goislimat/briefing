312527496dcb395adda957de5558bf62cdce8b78
Thiago Gois <goislimat2@gmail.com>
2018-02-02 22:09:37 -0200

    Layout de question reduzido feito (serão de programador)

--- client/src/components/pages/questions/components/QuestionCard.js
+++ client/src/components/pages/questions/components/QuestionCard.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import { CardGutter, Card, Controls } from './styles';
+import { CardGutter, Card, Controls, Button } from './styles';
 
 class QuestionCard extends Component {
   state = {
@@ -15,19 +15,24 @@ class QuestionCard extends Component {
     const { isExpanded } = this.state;
 
     return (
-      <CardGutter
-        className={`${isExpanded ? 'col-xl-12' : 'col-xl-6'} row`}
-        onClick={this.handleExpandedChange}
-      >
-        <Card className="col-xl-9">{questionText}</Card>
-        <Controls className="col-xl-3">
-          <div>
-            <i className="fas fa-arrows-alt" />
-          </div>
-          <div>
-            <i className="fas fa-info" />
-          </div>
-        </Controls>
+      <CardGutter className={`${isExpanded ? 'col-xl-12' : 'col-xl-6'}`}>
+        <div className="row">
+          <Card className="col-xl">
+            <span className="question">{questionText}</span>
+          </Card>
+          <Controls className="col-xl-auto d-flex justify-content-end align-items-end flex-column">
+            <div className="mb-auto">
+              <Button className="move">
+                <i className="fas fa-arrows-alt" />
+              </Button>
+            </div>
+            <div className="">
+              <Button className="more-info" onClick={this.handleExpandedChange}>
+                <i className="fas fa-info" />
+              </Button>
+            </div>
+          </Controls>
+        </div>
       </CardGutter>
     );
   }
--- client/src/components/pages/questions/components/styles.js
+++ client/src/components/pages/questions/components/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import color from '../../../styles/colors';
+import BaseButton from '../../../styles/BaseButton';
 
 export const CardGutter = styled.div`
   padding: 20px;
@@ -8,7 +9,28 @@ export const CardGutter = styled.div`
 
 export const Card = styled.div`
   background-color: ${color.white()};
-  padding: 50px;
+  padding: 45px;
+  .question {
+    font-size 1.3rem;
+    font-weight: bold;
+  }
 `;
 
-export const Controls = styled.div``;
+export const Controls = styled.div`
+  background-color: ${color.white()};
+  padding: 0;
+`;
+
+export const Button = BaseButton.extend`
+  border: 0;
+  color: ${color.white()};
+  &.move {
+    background-color: ${color.menuDarkGray()};
+    cursor: move;
+    padding: 15px 20px;
+  }
+  &.more-info {
+    background-color: ${color.orange()};
+    padding: 10px 20px;
+  }
+`;
